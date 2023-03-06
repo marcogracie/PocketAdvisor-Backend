a6ab88e49beb7b5adf0f8219791ebf081197cddc
marcogracie <marco.gracie22@gmail.com>
2023-03-06 18:19:32 -0500

    wish the guide included the imports necessary

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import render_template
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
