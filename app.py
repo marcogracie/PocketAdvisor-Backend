e932acc092e6b7752db6146dbf47c0513aeca8a7
marcogracie <marco.gracie22@gmail.com>
2023-03-06 21:38:12 -0500

    I cannot for the life of me figure out what is wrong with either the Oauth api call, or its response, going to comeback to it wednesday

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from firebase_admin import firestore
 from decouple import config
 import requests
 import json
+import sys
 
 app = Flask(__name__)
 cred = credentials.Certificate('/Users/marcogracie/Documents/Personal/Code/StockMarketApp/PocketAdvisor-Backend/pocket-advisor-d79c2-firebase-adminsdk-23bkk-d0397417ea.json')
@@ -17,6 +18,7 @@ db = firestore.client()
 
 client_code = "c2dc99c563f2b980bdb3895065b25c38"
 client_secret = config('secret_key', default='')
+
 
 @app.route("/main", methods=['GET', 'POST'])
 def main_page():
@@ -31,6 +33,7 @@ def main_page():
         head = {"Content-Type" : "application/x-www-form-urlencoded"}
         data = {"grant_type": "authorization_code", "code" : code, "client_id": client_code, "client_secret": client_secret, "redirect_uri": "http://127.0.0.1:5000/main"}
         access_data = requests.post("https://api.alpaca.markets/oauth/token", data = data, headers = head)
+        print(access_data, file=sys.stderr)
         access_token = json.loads(access_data.text)["access_token"]
         head = {"Authorization": "Bearer " + access_token}
         polygon_data = json.loads(requests.get("https://api.alpaca.markets/oauth/token", headers=head).text)
@@ -42,7 +45,6 @@ def main_page():
             "polygonToken": polygon_data["id"]
         }
 
-        # db.child("tokens").child(access_token).set(userData)
         db.collection('tokens').document(access_token).set(userData)
         return "Signup Finished. Please navigate back to the app and login!"
     
